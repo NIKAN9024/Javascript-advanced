85c3f5e8155b5aa88e5f591fdfb729a6e488c763
NIKAN9024 <9024387@student.tcrmbo.nl>
2025-09-15 14:25:51 +0200

    Combinatie van Array Methods

--- 1-js-methods/level-1-basis-methods/1.3-includes-oefening/app.js
+++ 1-js-methods/level-1-basis-methods/1.3-includes-oefening/app.js
@@ -9,6 +9,6 @@ console.log('Gezochte kleur:', zoekKleur);
 
 // TODO: Check of de kleur in de array staat
 const gevonden =
-  /* jouw code hier */
+  personen.find(persoon => persoon.leeftijd > 25);
 
   console.log('Kleur gevonden:', gevonden);
