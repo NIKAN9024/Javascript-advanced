08829c922bb7fc399dedde4fb255b3d7287b85c6
NIKAN9024 <9024387@student.tcrmbo.nl>
2025-09-23 11:59:27 +0200

    opdracht-01-arrow-loop

--- 2-objecten/opdracht-01-arrow-loop/app.js
+++ 2-objecten/opdracht-01-arrow-loop/app.js
@@ -1,4 +1,17 @@
 // Je gaat even oefenen met het gebruiken van een arrow function en de for/of loop. 
+console.log('opdracht 2.1: arrow function en for/of loop');
+
+
+let marvels = ["Ironman 1, Ironman 2, Ironman 3, Ironman 4"];
+const addMarvel = (newMarvel) => {
+    marvels.push(newMarvel);    
+}
+
+addMarvel("Ironman5")
+ for  (const marvel of marvels) {                  
+    console.log(marvel);
+ }
+
 
 // Maak een array genaamd 'marvels' en plaats hier 4 Marvel films in.
 
@@ -6,3 +19,4 @@
 // In je functie voeg je een nieuwe Marvel film toe zonder dit zelf in de bovenstaande array toe te voegen
 // Roep de functie op (het klopt dat je nog niets ziet)
 // Loop met een for/of loop door de array en toon alle items in de array in je console
+   
